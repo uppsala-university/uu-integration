111b5ff76cb1c1191ef2e1b6c36b0e2de21a19e4
markus <markus@jardemalm.se>
2015-02-11 14:06:21 +0100

    Added paths and methods for new event domains.

--- akkarest-event-adapter/src/main/java/se/uu/its/integration/service/identity/rest/IdentityService.java
+++ akkarest-event-adapter/src/main/java/se/uu/its/integration/service/identity/rest/IdentityService.java
@@ -6,15 +6,38 @@ import javax.ws.rs.core.Response;
 
 import se.uu.its.integration.model.common.UUEvent;
 import se.uu.its.integration.model.events.AffiliationEvent;
+import se.uu.its.integration.model.events.OrganizationDepartmentMappingEvent;
+import se.uu.its.integration.model.events.PersonEvent;
+import se.uu.its.integration.model.events.RoleEvent;
 
 @Path("/identity/")
 public class IdentityService {
 
     @POST
-    @Path("/event/")
+    @Path("/event/affiliation/")
     @Consumes(MediaType.APPLICATION_XML)
-    public Response putIdentityEvent(AffiliationEvent event) {
+    public Response putIdentityAffiliationEvent(AffiliationEvent event) {
         return null;
     }	
-	
+
+    @POST
+    @Path("/event/person/")
+    @Consumes(MediaType.APPLICATION_XML)
+    public Response putIdentityPersonEvent(PersonEvent event) {
+        return null;
+    }    
+    
+    @POST
+    @Path("/event/role/")
+    @Consumes(MediaType.APPLICATION_XML)
+    public Response putIdentityRoleEvent(RoleEvent event) {
+        return null;
+    } 
+    
+    @POST
+    @Path("/event/orgdepmap/")
+    @Consumes(MediaType.APPLICATION_XML)
+    public Response putIdentityOrganizationDepartmentMappingEvent(OrganizationDepartmentMappingEvent event) {
+        return null;
+    }    
 }
