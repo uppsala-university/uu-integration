68106fd602c0f8a3f65c3f84d90531f02e6416e2
markus <markus@jardemalm.se>
2015-02-13 15:18:20 +0100

    ... and the path continues...

---
+++ esb-model/src/main/java/se/uu/its/integration/model/service/identity/rest/IdentityService.java
@@ -0,0 +1,47 @@
+package se.uu.its.integration.service.identity.rest;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import se.uu.its.integration.model.common.UUEvent;
+import se.uu.its.integration.model.events.AffiliationEvent;
+import se.uu.its.integration.model.events.OrganizationDepartmentMappingEvent;
+import se.uu.its.integration.model.events.PersonEvent;
+import se.uu.its.integration.model.events.RoleEvent;
+
+@Path("/identity/")
+public class IdentityService {
+
+    @POST
+    @Path("/event/affiliation/")
+    @Consumes(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_XML)
+    public Response putIdentityAffiliationEvent(AffiliationEvent event) {
+        return null;
+    }	
+
+    @POST
+    @Path("/event/person/")
+    @Consumes(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_XML)
+    public Response putIdentityPersonEvent(PersonEvent event) {
+        return null;
+    }    
+    
+    @POST
+    @Path("/event/role/")
+    @Consumes(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_XML)
+    public Response putIdentityRoleEvent(RoleEvent event) {
+        return null;
+    } 
+    
+    @POST
+    @Path("/event/orgdepmap/")
+    @Consumes(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_XML)
+    public Response putIdentityOrganizationDepartmentMappingEvent(OrganizationDepartmentMappingEvent event) {
+        return null;
+    }    
+}
