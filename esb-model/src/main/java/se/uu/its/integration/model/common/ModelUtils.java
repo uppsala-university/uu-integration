b911c5fca42d0e88f16213821bb02db8298848fc
Henrik Eriksson <henrik.eriksson@uadm.uu.se>
2015-06-01 10:07:53 +0200

    Removing unused Sun logging file

--- esb-model/src/main/java/se/uu/its/integration/model/common/ModelUtils.java
+++ esb-model/src/main/java/se/uu/its/integration/model/common/ModelUtils.java
@@ -18,8 +18,6 @@ import javax.xml.transform.stream.StreamSource;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import com.sun.media.jfxmedia.logging.Logger;
-
 import se.uu.its.integration.model.events.UUEvent;
 
 public class ModelUtils {
