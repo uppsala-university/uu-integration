cfc28f61d4b253d8b81af910a40939da23173b1e
markus <markus@jardemalm.se>
2015-11-11 21:22:57 +0100

    Added new group namespace to package info.

--- esb-model/src/main/java/se/uu/its/integration/model/common/package-info.java
+++ esb-model/src/main/java/se/uu/its/integration/model/common/package-info.java
@@ -1,7 +1,8 @@
 @XmlSchema(
 	xmlns = {
 			@javax.xml.bind.annotation.XmlNs(prefix = "uuie", namespaceURI="http://www.uu.se/schemas/integration/2015/Events"),
-			@javax.xml.bind.annotation.XmlNs(prefix = "uuii", namespaceURI="http://www.uu.se/schemas/integration/2015/Identity")
+			@javax.xml.bind.annotation.XmlNs(prefix = "uuii", namespaceURI="http://www.uu.se/schemas/integration/2015/Identity"),
+			@javax.xml.bind.annotation.XmlNs(prefix = "uuig", namespaceURI="http://www.uu.se/schemas/integration/2015/Group")
 		   },
 		   
     namespace = "http://www.uu.se/schemas/integration/2015/Events", 
