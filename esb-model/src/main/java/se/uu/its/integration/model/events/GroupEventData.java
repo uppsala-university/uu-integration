ceefc4b5436ee760ca55dde2f1e6cb35156c8980
markus <markus@jardemalm.se>
2015-11-23 15:12:28 +0100

    Added new membership to event data.

--- esb-model/src/main/java/se/uu/its/integration/model/events/GroupEventData.java
+++ esb-model/src/main/java/se/uu/its/integration/model/events/GroupEventData.java
@@ -9,8 +9,9 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 import se.uu.its.integration.model.common.UUEventData;
 import se.uu.its.integration.model.common.UUEventDataProperty;
+import se.uu.its.integration.model.identity.Person;
 
-@XmlRootElement(name = "AffiliationEventData", namespace = "http://www.uu.se/schemas/integration/2015/Events")
+@XmlRootElement(name = "GroupEventData", namespace = "http://www.uu.se/schemas/integration/2015/Events")
 public class GroupEventData extends UUEventData{
 
 	private static final long serialVersionUID = 1788902282607510420L;
@@ -18,12 +19,22 @@ public class GroupEventData extends UUEventData{
 	@XmlElementWrapper(name = "GroupEventDataProperties", namespace = "http://www.uu.se/schemas/integration/2015/Events")	
 	@XmlElement(name = "EventDataProperty", namespace = "http://www.uu.se/schemas/integration/2015/Events")
 	List<UUEventDataProperty> eventDataProperties  = new ArrayList<UUEventDataProperty>();
+
+	@XmlElement(name = "NewMember", namespace = "http://www.uu.se/schemas/integration/2015/Identity")
+	Person newMembership = null;
 	
 	public GroupEventData() {
+	}
+	
+	public GroupEventData(Person member) {
+		this.newMembership = member;
 	}
 	
 	public void addEventPropertyData(UUEventDataProperty eventDataProperty) {
 		eventDataProperties.add(eventDataProperty);
 	}	
 	
+	public void addNewMember(Person member) {
+		this.newMembership = member;
+	}
 }
