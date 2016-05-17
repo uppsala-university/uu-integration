7dc24fbda241988102ec683931b18e0955a4b871
Henrik Eriksson <henrik.eriksson@uadm.uu.se>
2016-05-17 07:20:57 +0200

    If interval is too large, batch updates to each day

--- ladok2-groups/src/main/java/se/uu/its/integration/ladok2groups/Ladok2Groups.java
+++ ladok2-groups/src/main/java/se/uu/its/integration/ladok2groups/Ladok2Groups.java
@@ -44,20 +44,12 @@ public class Ladok2Groups {
 	NamedParameterJdbcTemplate l2Jdbc;
 	
 	public Date getLadok2GroupEventStartTime() {
-		return ladok2GroupEventStart;
-	}
-	
-	public static void main(String[] args) throws ParseException {
-		Date d = new Date();
-		d = DATE_FORMAT.parse("2016-01-01 080105");
-		Calendar c = new GregorianCalendar();
-		c.clear();
-		c.setTimeInMillis(d.getTime() + 86400000L);
-		c.set(Calendar.MILLISECOND, 0);
-		int ms = c.get(Calendar.MILLISECOND);
-		int ss = c.get(Calendar.SECOND);
-		System.out.println(DATE_FORMAT.format(d) + ", ms=" + ms + ", ss=" + ss
-				+ ", t=" + d.getTime() + ", c=" + c.getTimeInMillis() + ", cf=" + DATE_FORMAT.format(c.getTime()));
+		try { // TODO: DEBUG
+			return DATE_FORMAT.parse("2015-11-01 000000");
+		} catch (ParseException e) {
+			return null;
+		}
+		//return ladok2GroupEventStart;
 	}
 	
 	public String getLadok2GroupEventStart() {
@@ -85,44 +77,58 @@ public class Ladok2Groups {
 		if (l2Jdbc == null) { l2Jdbc = new NamedParameterJdbcTemplate(ladok2ReadDs); }
 	}
 	
-	public String updateGroupEvents() throws Exception {
+	public void updateGroupEvents() throws Exception {
 		init(); // delayed init to let the ds resources have time to be injected
 		List<MembershipEvent> pmes = query(esbJdbc, MembershipEvent.class,
 				esbSql.getMostRecentPotentialMembershipEventSql());
 		// Skip forward 1 second from most recent event to avoid duplicate events:
-		Date from = pmes.isEmpty() ? getLadok2GroupEventStartTime() : new Date(
+		Date start = pmes.isEmpty() ? getLadok2GroupEventStartTime() : new Date(
 				pmes.get(0).getDate().getTime() + 1000);
 		// Skip most recent events to make sure all events for this interval have arrived at Ladok:
-		Date to = new Date(new Date().getTime() - 15000);
-		int numMEs = updatePotentialMembershipEvents(from, to);
-		updateMembershipEvents();
-		return "Number of new Ladok membership events: " + numMEs;
-	}
-	
-	int updatePotentialMembershipEvents(Date from, Date to) {
-		// TODO: Iterate over days between from/to?
-		long time = from.getTime(), end = to.getTime(), dayMS = 86400000L;
-		Calendar c = new GregorianCalendar();
-		c.clear();
-		c.setTimeInMillis(time);
-		c.set(Calendar.MILLISECOND, 0);
-		time = c.getTimeInMillis();
-		// while (time < to) {
-		    // TODO
-		//}
+		Date end = new Date(new Date().getTime() - 15000);
 		
-		List<MembershipEvent> mes =  getNewLadokMembershipEvents(from, to); // TODO: max num
-		if (mes.size() > 10) {
-			mes =  mes.subList(0, 10);
+		// TODO: See below
+		//int numMEs = updatePotentialMembershipEvents(start, end);
+		//updateMembershipEvents();
+		
+		// If interval is too large, batch updates to each day:
+		Calendar from = new GregorianCalendar();
+		from.setTimeInMillis(start.getTime());
+		from.set(Calendar.MILLISECOND, 0);
+		Calendar to = new GregorianCalendar();
+		to.setTimeInMillis(end.getTime());
+		to.set(Calendar.MILLISECOND, 0);
+		int endYear = to.get(Calendar.YEAR);
+		int endDay = to.get(Calendar.DAY_OF_YEAR);
+		to.setTime(from.getTime());
+		to.set(Calendar.HOUR_OF_DAY, 23);
+		to.set(Calendar.MINUTE, 59);
+		to.set(Calendar.SECOND, 59);
+
+		// Update events for all days up to the last day
+		while (to.after(from) && (to.get(Calendar.YEAR) < endYear || to.get(Calendar.DAY_OF_YEAR) < endDay)) {
+			updatePotentialMembershipEvents(from.getTime(), to.getTime());
+			updateMembershipEvents();
+			// Increment interval one day:
+			from.setTime(to.getTime());
+			to.add(Calendar.DAY_OF_YEAR, 1);
 		}
+		// Update events for the last day
+		updatePotentialMembershipEvents(from.getTime(), end);
+		updateMembershipEvents();
+	}
+		
+	int updatePotentialMembershipEvents(Date from, Date to) {
+		List<MembershipEvent> mes =  getNewLadokMembershipEvents(from, to);
 		update(esbJdbc, esbSql.getSaveNewPotentialMembershipEventSql(), mes);
+		log.info("Updated potential membership events in interval [" 
+				+ DATE_FORMAT.format(from) + ", " + DATE_FORMAT.format(to) + "): " + mes.size());
 		return mes.size();
 	}
 	
 	int updateMembershipEvents() {
 		List<MembershipEvent> potentialMembershipEvents = getUnprocessedPotentialMembershipEvents();
-		//List<MembershipEvent> mes = new ArrayList<MembershipEvent>();
-		log.info("Unprocessed mes: " + potentialMembershipEvents);
+		log.info("Number of unprocessed potential membership events: " + potentialMembershipEvents.size());
 		for (MembershipEvent me : potentialMembershipEvents) {
 			if (me.getMeType() == MembershipEvent.Type.ADD) {
 				// TODO: fortsattningsreg -> update membership ?
@@ -150,14 +156,21 @@ public class Ladok2Groups {
 	}
 	
 	List<MembershipEvent> getNewLadokMembershipEvents(Date from, Date to) {
-		String fd = MembershipEventUtil.DATE_FORMAT.format(from);
-		String[] dateAndTime = fd.split(" ");
-		String date = dateAndTime[0];
-		String time = dateAndTime[1];
-		List<Reg> reg = query(l2Jdbc, Reg.class, l2Sql.getRegSql(), "datum", date, "tid", time);
-		List<BortReg> bortreg = query(l2Jdbc, BortReg.class, l2Sql.getBortRegSql(), "datum", date, "tid", time);
-		List<InReg> inreg = query(l2Jdbc, InReg.class, l2Sql.getInRegSql(), "datum", date, "tid", time);
-		List<Avliden> avliden = query(l2Jdbc, Avliden.class, l2Sql.getAvlidenSql(), "datum", date);
+		String d_to = MembershipEventUtil.DATE_FORMAT.format(to);
+		String[] dt_to = d_to.split(" ");
+		String date_to = dt_to[0];
+		String d_from = MembershipEventUtil.DATE_FORMAT.format(from);
+		String[] dt_from = d_from.split(" ");
+		String date_from = dt_from[0];
+		String time_from = date_from.equals(date_to) ? dt_from[1] : "000000";
+		List<Reg> reg = query(l2Jdbc, Reg.class, l2Sql.getRegSql(), 
+				"datum_from", date_from, "datum_to", date_to, "tid", time_from);
+		List<BortReg> bortreg = query(l2Jdbc, BortReg.class, l2Sql.getBortRegSql(), 
+				"datum_from", date_from, "datum_to", date_to, "tid", time_from);
+		List<InReg> inreg = query(l2Jdbc, InReg.class, l2Sql.getInRegSql(), 
+				"datum_from", date_from, "datum_to", date_to, "tid", time_from);
+		List<Avliden> avliden = query(l2Jdbc, Avliden.class, l2Sql.getAvlidenSql(), 
+				"datum_from", date_from, "datum_to", date_to);
 		List<MembershipEvent> mes = new ArrayList<MembershipEvent>();
         mes.addAll(MembershipEventUtil.toMembershipEvents(reg));
         mes.addAll(MembershipEventUtil.toMembershipEvents(bortreg));
