b94a3dfc9cf4242eca9aac9defa32f310b24e126
Henrik Eriksson <henrik.eriksson@uadm.uu.se>
2017-04-19 16:30:50 +0200

    Adding PNR util to calculate the tow century digits for 10-figure PNRs

---
+++ ladok2-groups-standalone/src/main/java/se/uu/its/integration/ladok2groups/util/PnrUtil.java
@@ -0,0 +1,25 @@
+package se.uu.its.integration.ladok2groups.util;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import org.springframework.util.StringUtils;
+
+public class PnrUtil {
+	
+	public static Calendar cal = new GregorianCalendar();
+	
+	public static String normalize(String pnr) {
+		if (StringUtils.isEmpty(pnr)) return null;
+		String nPnr = pnr.replaceAll("[ -]", "");
+		if (nPnr.length() == 10) {
+			int cent = cal.get(Calendar.YEAR) / 100;
+			int yr = cal.get(Calendar.YEAR) % 100;
+			int pnrYr = Integer.parseInt(nPnr.substring(0, 2));
+			// Interpret ages < 10 year old as 100 year older
+			int pnrCent = (yr-pnrYr < 10) ? cent-1 : cent;
+			nPnr = pnrCent + nPnr;
+		}
+		return "'"+nPnr+"'";
+	}
+}
