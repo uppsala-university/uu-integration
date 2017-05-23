367ee36ec6d60eb52f6135d553be6fd9b2a6adfe
Henrik Eriksson <henrik.eriksson@uadm.uu.se>
2017-05-23 15:20:24 +0200

    LTI-100, AterbudEvent för antagna som tackar nej (eller inte finns med i antagningstabellen längre)

--- ladok2-groups-standalone/src/main/java/se/uu/its/integration/ladok2groups/service/AcceptedEventService.java
+++ ladok2-groups-standalone/src/main/java/se/uu/its/integration/ladok2groups/service/AcceptedEventService.java
@@ -23,6 +23,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import se.uu.its.integration.ladok2groups.dto.AccMembership;
 import se.uu.its.integration.ladok2groups.dto.GroupEvent;
 import se.uu.its.integration.ladok2groups.dto.MembershipEvent;
+import se.uu.its.integration.ladok2groups.dto.PotentialMembershipEvent.Type;
 import se.uu.its.integration.ladok2groups.l2dto.Antagen;
 import se.uu.its.integration.ladok2groups.sql.EsbGroupSql;
 import se.uu.its.integration.ladok2groups.sql.Ladok2GroupSql;
@@ -47,7 +48,6 @@ public class AcceptedEventService {
 	
 	@Scheduled(cron = "${events.accUpdateCron}")
 	public void updateAccepted() {
-		// System.out.println("updateAccepted: " + format(new Date()));
 		long start = System.currentTimeMillis();
 		// TODO: Include prev and/or next semester too?:
 		String semester = queryByParams(l2Jdbc, String.class, l2Sql.getTerminSql()).get(0);
@@ -61,15 +61,19 @@ public class AcceptedEventService {
 		List<AccMembership> l2Accs = MembershipEventUtil.toAccMemberships(
 				l2Ant, eventDate);
 		long save = System.currentTimeMillis();
+		// Add membership events:
 		List<AccMembership> addedAccs = new ArrayList<AccMembership>(l2Accs);
 		addedAccs.removeAll(storedAccs);
 		List<MembershipEvent> membershipAddEvents = MembershipEventUtil.toMembershipAddEvents(addedAccs);
-		/*
+		// Remove membership events:
 		List<AccMembership> removedAccs = new ArrayList<AccMembership>(storedAccs);
 		removedAccs.removeAll(l2Accs);
 		List<MembershipEvent> membershipRemoveEvents = MembershipEventUtil.toMembershipRemoveEvents(removedAccs);
-		*/
-		saveAccMemberEvents(l2Accs, membershipAddEvents);
+		// All membership events:
+		List<MembershipEvent> allMembershipEvents = new ArrayList<MembershipEvent>(membershipAddEvents);
+		allMembershipEvents.addAll(membershipRemoveEvents);
+		// saveAccMemberEvents(l2Accs, membershipAddEvents);
+		saveAccMemberEvents(l2Accs, allMembershipEvents);
 		log.info("Accepted membership events update: " 
 				+ "Stored: " + storedAccs.size()
 				+ ", Ladok: " + l2Accs.size()
@@ -80,9 +84,9 @@ public class AcceptedEventService {
 				+ " , save: " + (save - l2Read) + ")");
 	}
 	
-	void saveAccMemberEvents(List<AccMembership> memberships, List<MembershipEvent> membAddEvents) {
+	void saveAccMemberEvents(List<AccMembership> memberships, List<MembershipEvent> membEvents) {
 		List<GroupEvent> groupEvents = new ArrayList<>();
-		Set<String> cis = getUniqueCourseInstances(membAddEvents);
+		Set<String> cis = getUniqueCourseInstances(membEvents);
 		for (String ci : cis) {
 			String[] split = ci.split(":");
 			String courseCode = split[0];
@@ -93,14 +97,14 @@ public class AcceptedEventService {
 					"startSemester", startSemester,
 					"reportCode", reportCode);
 			if (Integer.valueOf(0).equals(numRegs.get(0))) {
-				MembershipEvent me = membAddEvents.get(0);
+				MembershipEvent me = membEvents.get(0);
 				groupEvents.add(new GroupEvent(me.getDate(), courseCode,
 						startSemester, reportCode, me.getOrigin()));
 			}
 		}
 		executeStatementsInSameTx(esbJdbc, esbTm,
 				sqlAndVals(esbSql.getSaveNewMembershipEventSql(), groupEvents),
-				sqlAndVals(esbSql.getSaveNewMembershipEventSql(), membAddEvents),
+				sqlAndVals(esbSql.getSaveNewMembershipEventSql(), membEvents),
 				sql(esbSql.getDeleteAccMembershipsSql()),
 				sqlAndVals(esbSql.getSaveNewAccMembershipSql(), memberships));
 	}
@@ -108,6 +112,7 @@ public class AcceptedEventService {
 	Set<String> getUniqueCourseInstances(List<MembershipEvent> mes) {
 		Set<String> cis = new HashSet<String>();
 		for (MembershipEvent me : mes) {
+			if (me.getMeType() != Type.ForvantatDeltagandeSkapadEvent) continue;
 			cis.add(me.getCourseCode() + ":" + me.getStartSemester() + ":"
 					+ me.getReportCode());
 		}
